27f279571f977e73459358adfc9786aa8f568edf
Ferraz Junior, N <norisjunior@gmail.com>
2022-01-04 11:09:28 -0300

    env.js endereço

--- docker/app/env.js
+++ docker/app/env.js
@@ -1,7 +1,7 @@
 (function (window) {
     window.__env = window.__env || {};
-    window.__env.identityUrl = "/gateway/",
-    window.__env.apiGatewayUrl = "/gateway/",
+    window.__env.identityUrl = "http://api-gateway/gateway",
+    window.__env.apiGatewayUrl = "http://api-gateway/gateway",
     window.__env.modo = "normal", // "avancado",
     window.__env.client = {
         id: "SISGP.Web",
--- install/Arquivos de configuração/3. Susep.SISRH.WebApp/ClientApp/dist/env.js
+++ install/Arquivos de configuração/3. Susep.SISRH.WebApp/ClientApp/dist/env.js
@@ -1,7 +1,7 @@
 (function (window) {
     window.__env = window.__env || {};
-    window.__env.identityUrl = "https://sitedoorgao/sisgp/gateway/",
-    window.__env.apiGatewayUrl = "https://sitedoorgao/sisgp/gateway/",
+    window.__env.identityUrl = "http://api-gateway/gateway",
+    window.__env.apiGatewayUrl = "http://api-gateway/gateway",
     window.__env.modo = "normal", // "avancado",
     window.__env.client = {
         id: "SISGP.Web",
